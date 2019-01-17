0010a6ec90a53123c13afa8a78579a9cd31fecc3
zhangzhicheng <793338023@qq.com>
2019-01-17 23:37:04 +0800

    初始

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="hello">
+    <div>{{msg}}</div>
+  </div>
+</template>
+
+<script>
+import abc from './abc';
+import skeleton from '../skeleton/Skeleton.vue';
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'xxxxxxxx',
+      abc: 'abc',
+      obj:{
+        a:"ccc"
+      },
+      arr:["x","c","d"]
+    }
+  },
+  methods:{
+    parantFn(pp){
+      this.abc = pp;
+      console.log("父组件。。。");
+    },
+    clickFn(){
+      this.abc = "zhangzhicheng";
+      this.obj.bbbb = "bbb";
+      this.arr[5]="555";
+      let a="aa";
+      let obj={
+        [a]:"xxx"
+      };
+    },
+    testFn(a,b,c,d,e){
+    }
+  },
+  computed:{
+
+  },
+  components:{
+    abc
+  },
+  created(){
+
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='less'>
+.hello /deep/ ul{
+    list-style-type: none;
+    padding: 0;
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+}
+.hello{
+  h1, h2 {
+    font-weight: normal;
+  }
+}
+</style>
---
+++ src/components/abc.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <div ref="ic" @click="clickFn">xhxhxh</div>
+    <p>{{abc}}</p> 
+    <p>{{obj.bbbb}}</p> 
+    <p>{{arr[5]}}</p> 
+    <p>{{abc2}}</p> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '子页面',
+      abc: 'abc',
+      obj:{
+        a:"ccc"
+      },
+      arr:["x","c","d"]
+    }
+  },
+  props:{
+    abc2:{
+      default:"xxx",
+      type: String
+    }
+  },
+  methods:{
+
+    clickFn(){
+    },
+    testFn(a,b,c,d,e){
+
+    }
+  },
+  computed:{
+
+  },
+  components:{
+
+  },
+  created(){
+    
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='less'>
+.hello /deep/ ul{
+    list-style-type: none;
+    padding: 0;
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+}
+.hello{
+  h1, h2 {
+    font-weight: normal;
+  }
+}  
+</style>
